8ea19247cf542fbeedb5939b0c831cbf8b9a652f
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2020-08-11 15:31:57 +0300

    finsihed it

--- js/app.js
+++ js/app.js
@@ -1,16 +1,61 @@
 'use strict'
 let images = []
+let secondPageImages = []
 let imageRendering = document.getElementById('render')
-let imageFiltering = document.getElementById('filter')
+let imageFiltering = document.getElementById('filterFirstPage')
+let imageFilteringSecond = document.getElementById('filterSecondPage')
 let imageSorting = document.getElementById('sort')
+let firstPage = document.getElementById('page1')
+let secondPage = document.getElementById('page2')
 fetch("../data/page-1.json")
   .then((results) => results.json())
   .then((data) => {
     data.forEach((value) => {
       images.push(value)
     })
+    imageFilteringSecond.remove()
     render()
+    imageSorting.addEventListener('change', (e) => {
+      if (e.target.value === 'Title') {
+        let titleCopy = images.slice()
+        let sortedTitle = titleCopy.sort(sortedByTitle)
+        imageRendering.innerHTML = ''
+        sortedTitle.forEach((val) => {
+          let imageEl = document.createElement('img')
+          imageRendering.appendChild(imageEl)
+          imageEl.setAttribute('src', val.image_url)
+        })
+      } else if (e.target.value === 'Horn') {
+        let hornCopy = images.slice()
+        let sortedHorn = hornCopy.sort(sortedByHorn)
+        imageRendering.innerHTML = ''
+        sortedHorn.forEach((val) => {
+          let imageEl = document.createElement('img')
+          imageRendering.appendChild(imageEl)
+          imageEl.setAttribute('src', val.image_url)
+        })
+      } else if (e.target.value === 'Sort-By-Title / Horn') {
+        imageRendering.innerHTML = ''
+        render()
+      }
+
+    })
   })
+
+fetch('../data/page-2.json')
+  .then((results) => results.json())
+  .then((data) => {
+    data.forEach((val) => {
+      secondPageImages.push(val)
+    })
+  })
+function renderSecondPage() {
+  secondPageImages.forEach((value) => {
+    let imgEl = document.createElement("img");
+    imageRendering.appendChild(imgEl);
+    imgEl.setAttribute('src', value.image_url)
+  });
+}
 function render() {
   images.forEach((value) => {
     let imgEl = document.createElement("img");
@@ -18,6 +63,66 @@ function render() {
     imgEl.setAttribute('src', value.image_url)
   });
 }
+secondPage.addEventListener('click', () => {
+  imageRendering.innerHTML = ''
+  imageFiltering.replaceWith(imageFilteringSecond)
+  renderSecondPage()
+  imageSorting.addEventListener('change', (e) => {
+    if (e.target.value === 'Title') {
+      let titleCopy = secondPageImages.slice()
+      let sortedTitle = titleCopy.sort(sortedByTitle)
+      imageRendering.innerHTML = ''
+      sortedTitle.forEach((val) => {
+        let imageEl = document.createElement('img')
+        imageRendering.appendChild(imageEl)
+        imageEl.setAttribute('src', val.image_url)
+      })
+    } else if (e.target.value === 'Horn') {
+      let hornCopy = secondPageImages.slice()
+      let sortedHorn = hornCopy.sort(sortedByHorn)
+      imageRendering.innerHTML = ''
+      sortedHorn.forEach((val) => {
+        let imageEl = document.createElement('img')
+        imageRendering.appendChild(imageEl)
+        imageEl.setAttribute('src', val.image_url)
+      })
+    } else if (e.target.value === 'Sort-By-Title / Horn') {
+      imageRendering.innerHTML = ''
+      render()
+    }
+
+  })
+})
+firstPage.addEventListener('click', () => {
+  imageRendering.innerHTML = ''
+  imageFilteringSecond.replaceWith(imageFiltering)
+  render()
+  imageSorting.addEventListener('change', (e) => {
+    if (e.target.value === 'Title') {
+      let titleCopy = images.slice()
+      let sortedTitle = titleCopy.sort(sortedByTitle)
+      imageRendering.innerHTML = ''
+      sortedTitle.forEach((val) => {
+        let imageEl = document.createElement('img')
+        imageRendering.appendChild(imageEl)
+        imageEl.setAttribute('src', val.image_url)
+      })
+    } else if (e.target.value === 'Horn') {
+      let hornCopy = images.slice()
+      let sortedHorn = hornCopy.sort(sortedByHorn)
+      imageRendering.innerHTML = ''
+      sortedHorn.forEach((val) => {
+        let imageEl = document.createElement('img')
+        imageRendering.appendChild(imageEl)
+        imageEl.setAttribute('src', val.image_url)
+      })
+    } else if (e.target.value === 'Sort-By-Title / Horn') {
+      imageRendering.innerHTML = ''
+      render()
+    }
+
+  })
+})
 imageFiltering.addEventListener('change', (e) => {
   imageRendering.innerHTML = ''
   images.forEach((val) => {
@@ -31,31 +136,22 @@ imageFiltering.addEventListener('change', (e) => {
     }
   });
 })
-imageSorting.addEventListener('change', (e) => {
-  if (e.target.value === 'Title') {
-    let titleCopy = images.slice()
-    let sortedTitle = titleCopy.sort(sortedByTitle)
-    imageRendering.innerHTML = ''
-    sortedTitle.forEach((val) => {
-      let imageEl = document.createElement('img')
-      imageRendering.appendChild(imageEl)
-      imageEl.setAttribute('src', val.image_url)
-    })
-  } else if (e.target.value === 'Horn') {
-    let hornCopy = images.slice()
-    let sortedHorn = hornCopy.sort(sortedByHorn)
-    imageRendering.innerHTML = ''
-    sortedHorn.forEach((val) => {
-      let imageEl = document.createElement('img')
-      imageRendering.appendChild(imageEl)
-      imageEl.setAttribute('src', val.image_url)
-    })
-  } else if (e.target.value === 'Sort-By-Title / Horn') {
-    imageRendering.innerHTML = ''
-    render()
-  }
-
+imageFilteringSecond.addEventListener('change', (e) => {
+  imageRendering.innerHTML = ''
+  secondPageImages.forEach((val) => {
+    if (e.target.value === val.keyword) {
+      let imgEl = document.createElement("img");
+      imageRendering.appendChild(imgEl);
+      imgEl.setAttribute('src', val.image_url)
+    } else if (e.target.value === 'Filter-By-Keyword') {
+      imageRendering.innerHTML = ''
+      render()
+    }
+  });
 })
+
+
+
 function sortedByTitle(a, b) {
   if (a.title < b.title) {
     return -1;
@@ -74,3 +170,4 @@ function sortedByHorn(a, b) {
   }
   return 0;
 }
+
